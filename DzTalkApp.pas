c4fbd611512fe4aa60ac8b54212a0c9dac0d7b9c
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2019-03-10 15:00:40 -0300

    First commit

---
+++ DzTalkApp.pas
@@ -0,0 +1,257 @@
+{------------------------------------------------------------------------------
+TDzTalkApp component
+Developed by Rodrigo Depiné Dalpiaz (digao dalpiaz)
+Non visual component to communicate between applications
+
+https://github.com/digao-dalpiaz/DzTalkApp
+
+Please, read the documentation at GitHub link.
+------------------------------------------------------------------------------}
+
+{Send Record:
+ > DzTalkApp.Send(ID, @Record, SizeOf(Record));
+
+ Receive Record (OnMessage):
+ > Record := TRecord(P^);
+
+ Warning: always use "packed record" !!!
+
+ // -------------------------------------------
+
+ Send Double:
+ > DzTalkApp.Send(ID, @Dbl, SizeOf(Dbl));
+
+ Receive Double (OnMessage):
+ > Dbl := Double(P^);
+}
+
+unit DzTalkApp;
+
+interface
+
+uses System.Classes, Winapi.Windows, Winapi.Messages, System.SysUtils;
+
+type
+  TOnMessage = procedure(Sender: TObject; From: HWND; ID: Word; P: Pointer) of object;
+
+  TDzTalkApp = class(TComponent)
+  private
+    WinHandle: HWND; //Handle of VIRTUAL Window created by component
+
+    FAutoActivate: Boolean; //auto-activate on component Loaded
+    FAutoFind: Boolean; //auto-find destination Window on Send
+    FMyWindowName: String;
+    FDestWindowName: String;
+    FSynchronous: Boolean; //stay locked until other app release OnMessage event
+
+    FActive: Boolean;
+    FToHandle: HWND; //Destination Window Handle
+
+    FOnMessage: TOnMessage;
+
+    LastFrom: HWND; //last received handle
+    LastData: Pointer; //last data pointer
+
+    procedure WndProc(var Msg: TMessage);
+    procedure Msg_CopyData(D: TWMCopyData);
+
+    procedure IntEnv(ID: Word; P: Pointer; Size: Integer);
+    procedure SetMyWindowName(const Value: String);
+  protected
+    procedure Loaded; override;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
+    procedure Enable;
+    procedure Disable;
+
+    procedure FindDestWindow;
+
+    procedure Send(ID: Word); overload;
+    procedure Send(ID: Word; N: Integer); overload;
+    procedure Send(ID: Word; A: AnsiString); overload;
+    procedure Send(ID: Word; P: Pointer; Size: Integer); overload;
+
+    function AsString: AnsiString; //read received message as String
+    function AsInteger: Integer; //read received message as Integer
+
+    property Active: Boolean read FActive;
+    property ToHandle: HWND read FToHandle write FToHandle;
+  published
+    property AutoActivate: Boolean read FAutoActivate write FAutoActivate default False;
+    property AutoFind: Boolean read FAutoFind write FAutoFind default False;
+    property MyWindowName: String read FMyWindowName write SetMyWindowName;
+    property DestWindowName: String read FDestWindowName write FDestWindowName;
+    property Synchronous: Boolean read FSynchronous write FSynchronous default False;
+
+    property OnMessage: TOnMessage read FOnMessage write FOnMessage;
+  end;
+
+  EDzTalkAppWndNotFound = class(Exception);
+
+procedure Register;
+
+implementation
+
+uses Vcl.Forms;
+
+procedure Register;
+begin
+  RegisterComponents('Digao', [TDzTalkApp]);
+end;
+
+const CONST_WM = WM_COPYDATA; {!}
+
+constructor TDzTalkApp.Create(AOwner: TComponent);
+begin
+  inherited;
+
+  FActive := False;
+end;
+
+destructor TDzTalkApp.Destroy;
+begin
+  if FActive then Disable;
+
+  inherited;
+end;
+
+procedure TDzTalkApp.Loaded;
+begin
+  inherited;
+
+  if not (csDesigning in ComponentState) then
+    if FAutoActivate then Enable;
+end;
+
+procedure TDzTalkApp.Enable;
+begin
+  if FActive then
+    raise Exception.Create('TalkApp already active');
+
+  if FMyWindowName='' then
+    raise Exception.Create('Window name is blank');
+
+  WinHandle := AllocateHWND(WndProc);
+  SetWindowText(WinHandle, PChar(FMyWindowName));
+
+  FActive := True;
+end;
+
+procedure TDzTalkApp.Disable;
+begin
+  if not FActive then
+    raise Exception.Create('TalkApp is not active');
+
+  DeallocateHWnd(WinHandle);
+
+  FActive := False;
+end;
+
+procedure TDzTalkApp.SetMyWindowName(const Value: String);
+begin
+  if Value<>FMyWindowName then
+  begin
+    if FActive then
+      raise Exception.Create('Can''t change window name while handle is active');
+
+    FMyWindowName := Value;
+  end;
+end;
+
+procedure TDzTalkApp.WndProc(var Msg: TMessage);
+begin
+  if Msg.Msg = CONST_WM then //CopyData message
+  begin
+    if not FSynchronous then ReplyMessage(Msg.Result);
+
+    try
+      Msg_CopyData( TWMCopyData(Msg) );
+    except
+      Application.HandleException(Self);
+    end;
+
+    Msg.Result := 0;
+  end
+    else
+      Msg.Result := DefWindowProc(WinHandle, Msg.Msg, Msg.WParam, Msg.LParam);
+end;
+
+procedure TDzTalkApp.Msg_CopyData(D: TWMCopyData);
+var
+  From: HWND;
+  dI: Cardinal;
+  dP: Pointer;
+begin
+  From := D.From; {Msg.WParam}
+  dI := D.CopyDataStruct.dwData;
+  dP := D.CopyDataStruct.lpData;
+
+  LastFrom := From;
+  LastData := dP;
+
+  if Assigned(FOnMessage) then
+    FOnMessage(Self, From, dI, dP);
+end;
+
+procedure TDzTalkApp.IntEnv(ID: Word; P: Pointer; Size: Integer);
+var D: TCopyDataStruct;
+begin
+  D.dwData := ID;
+  D.cbData := Size;
+  D.lpData := P;
+
+  SendMessage(FToHandle, CONST_WM, WinHandle, Integer(@D));
+end;
+
+procedure TDzTalkApp.Send(ID: Word);
+begin
+  Send(ID, nil, 0);
+end;
+
+procedure TDzTalkApp.Send(ID: Word; N: Integer);
+begin
+  Send(ID, @N, SizeOf(N));
+end;
+
+procedure TDzTalkApp.Send(ID: Word; A: AnsiString);
+begin
+  Send(ID, PAnsiChar(A), Length(A)+1);
+end;
+
+procedure TDzTalkApp.Send(ID: Word; P: Pointer; Size: Integer);
+begin
+  if not FActive then
+    raise Exception.Create('TalkApp is not active to send');
+
+  if FAutoFind then FindDestWindow;
+
+  IntEnv(ID, P, Size);
+end;
+
+procedure TDzTalkApp.FindDestWindow;
+var H: HWND;
+begin
+  if FDestWindowName='' then
+    raise Exception.Create('Destination window name is blank');
+
+  H := FindWindow(nil, PChar(FDestWindowName));
+
+  if H<>0 then
+    FToHandle := H
+  else
+    raise EDzTalkAppWndNotFound.Create('Destination app window not found');
+end;
+
+function TDzTalkApp.AsString: AnsiString;
+begin
+  Result := PAnsiChar(LastData);
+end;
+
+function TDzTalkApp.AsInteger: Integer;
+begin
+  Result := Integer(LastData^);
+end;
+
+end.
---
+++ README.md
@@ -0,0 +1,177 @@
+# DzTalkApp
+
+## Delphi non-visual component to communicate between applications
+
+![Delphi Supported Versions](https://img.shields.io/badge/Delphi%20Supported%20Versions-XE2..10.3%20Rio-blue.svg)
+![Platforms](https://img.shields.io/badge/Platforms-Win32%20and%20Win64-red.svg)
+![Auto Install](https://img.shields.io/badge/-Auto%20Install%20App-orange.svg)
+
+- [Component Description](#component-description)
+- [Installing](#installing)
+- [How to use](#how-to-use)
+- [Properties](#properties)
+- [Methods](#methods)
+- [Events](#events)
+- [How to send and read custom types](#how-to-send-and-read-custom-types)
+
+## Component Description
+
+DzTalkApp allows you to send data between distinct applications. You can send simple data type like Integer or String, and even complex data, like a record.
+
+## Installing
+
+### Auto install
+
+Close Delphi IDE and run **CompInstall.exe** app to auto install component into Delphi.
+
+### Manual install
+
+1. Open **DzTalkApp** package in Delphi.
+2. Ensure **Win32** Platform and **Release** config are selected.
+3. Then **Build** and **Install**.
+4. If you want to use Win64 platform, select this platform and Build again.
+5. Add sub-path Win32\Release to the Library paths at Tools\Options using 32-bit option, and if you have compiled to 64 bit platform, add sub-path Win64\Release using 64-bit option.
+
+Supports Delphi XE2..Delphi 10.3 Rio
+
+## How to use
+
+Drop a TDzTalkApp in the source and destination applications.
+
+You just need to set the window name in the component, and the communication will occur between window handles.
+
+In the destination application, just set the OnMessage event to receive and read data.
+
+## Properties
+
+`AutoActivate: Boolean` = Auto-enable the handle on component loaded. This will only works when you set other properties in design-time mode.
+
+`AutoFind: Boolean` = When AutoFind is enabled, on each Send command, the component will find the destination window handle.
+
+`MyWindowName: String` = Specify the source handle name. This property is only needed if this app will receive communication. Otherwise you only need to set the destination window name.
+
+`DestWindowName: String` = Specify the destination window name. This property is only needed if this app will send communication, otherwise you only need to set the source window handle name.
+
+`Synchronous: Boolean` = If this property is enabled, when you send a command to other app, the execution pointer will be released only after the destination app method `OnMessage` ends. If this property is disabled, when the destination receive the message, immediately the execution pointer is released in the source application.
+
+`Active: Boolean` (public read-only) = Indicates if the component is enabled or disabled (see `Enable` and `Disable` methods).
+
+`ToHandle: HWND` (public) = Stores the last destination window handle. You can set this property manually if needed, but I recommend you to use `FindDestWindow` method instead.
+
+## Methods
+
+```delphi
+procedure Enable;
+```
+
+Enables the window handle to send and receive messages. You need to specify the name of window handle before use this method.
+
+```delphi
+procedure Disable;
+```
+
+Disables the window handle.
+
+```delphi
+procedure FindDestWindow;
+```
+
+Find and set destination window handle by the name defined in `DestWindowName` property. If the destination window is not found, an error is raised, using class `EDzTalkAppWndNotFound`.
+
+```delphi
+procedure Send(ID: Word); overload;
+procedure Send(ID: Word; N: Integer); overload;
+procedure Send(ID: Word; A: AnsiString); overload;
+procedure Send(ID: Word; P: Pointer; Size: Integer); overload;
+```
+
+Use **Send** methods to send a message to destination application. To send messages, the component needs to be enabled (use `Enable` method) and needs a destination window name defined (use `DestWindowName` property).
+
+The overloads methods allows you to send:
+
+- Only a command, using `ID` parameter
+
+- A command with an Integer parameter, using `N` parameter
+
+- A command with a String parameter, using `A` parameter
+
+- A command with any kind of data parameter, using `P` parameter and specifying the size of data parameter.
+
+```delphi
+function AsString: AnsiString;
+```
+
+Use this function inside the OnMessage event to get a message data as String type.
+
+```delphi
+function AsInteger: Integer;
+```
+
+Use this function inside the OnMessage event to get a message data as Integer type.
+
+## Events
+
+```delphi
+procedure OnMessage(Sender: TObject; From: HWND; ID: Word; P: Pointer);
+```
+
+This method will occur in the destination app when a message is received. The `From` parameter indicates the handle of the source window. The `ID` parameter indicates the code of the message used in the `Send` method at the source app. The `P` parameter has the pointer of the message data.
+
+You can use the `AsString` or `AsInteger` functions to read messages as String and Integer data types.
+
+## How to send and read custom types
+
+### Record types:
+
+You can send record structure. To do this, ensure you are always using `packed record`, because this kind of record will use a fixed size structure.
+
+**To send a record, use:**
+
+```delphi
+type
+  TData = packed record
+    Number: Integer;
+	Text: ShortString;
+	Flag: Boolean;
+  end;
+  
+var R: TData;
+begin
+  R.Number := 100;
+  R.Text := 'TEST';
+  R.Flag := True;
+  
+  DzTalkApp.Send(1, @R, SizeOf(R));
+end;
+```
+
+**To receive this record using OnMessage event:**
+
+```delphi
+var R: TData;
+begin
+  R := TData(P^);
+end;
+```
+
+> Of course, the packed record on destination application must have the same structure as the record used in the sender application.
+
+### Simple types:
+
+**To send a Double type:
+
+```delphi
+var D: Double;
+begin
+  DzTalkApp.Send(1, @D, SizeOf(D));
+end;
+```
+
+**To receive a Double type (OnMessage event):
+
+```delphi
+var D: Double;
+begin 
+  D := Double(P^);
+end;
+```
